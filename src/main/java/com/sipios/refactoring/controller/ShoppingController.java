7df2c11dcda393c996f8bfa25cde3db195fe97f1
Franckynours <franck.brazier@gmail.com>
2023-06-26 11:24:38 +0200

    refactor: split getPrice method

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -1,9 +1,11 @@
 package com.sipios.refactoring.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sipios.refactoring.service.ShoppingService;
 
@@ -16,7 +18,12 @@ public class ShoppingController {
 
     @PostMapping
     public String getPrice(@RequestBody Body b) {
-        // TODO convert Body to "metier" object
-        return String.valueOf(shoppingService.getPrice(b));
+
+        try {
+            // TODO convert Body to "metier" object
+            return String.valueOf(shoppingService.getPrice(b));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 }
--- src/main/java/com/sipios/refactoring/service/ShoppingService.java
+++ src/main/java/com/sipios/refactoring/service/ShoppingService.java
@@ -6,8 +6,6 @@ import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.sipios.refactoring.controller.Body;
 import com.sipios.refactoring.controller.Item;
@@ -16,53 +14,19 @@ public class ShoppingService {
 
     private Logger logger = LoggerFactory.getLogger(ShoppingService.class);
 
-    public double getPrice(Body b) {
+    public double getPrice(Body b) throws Exception {
         double p = 0;
-        double d;
-
-        Date date = new Date();
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
-        cal.setTime(date);
 
         // Compute discount for customer
-        if (b.getType().equals("STANDARD_CUSTOMER")) {
-            d = 1;
-        } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-            d = 0.9;
-        } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-            d = 0.5;
-        } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        double d = getCustomerDisount(b.getType());
+
+        if (b.getItems() == null) {
+            return 0;
         }
 
         // Compute total amount depending on the types and quantity of product and
         // if we are in winter or summer discounts periods
-        if (!(cal.get(Calendar.DAY_OF_MONTH) < 15 && cal.get(Calendar.DAY_OF_MONTH) > 5 && cal.get(Calendar.MONTH) == 5)
-                && !(cal.get(Calendar.DAY_OF_MONTH) < 15 && cal.get(Calendar.DAY_OF_MONTH) > 5
-                        && cal.get(Calendar.MONTH) == 0)) {
-            if (b.getItems() == null) {
-                return 0;
-            }
-
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
-
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * d;
-                }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                // price += 80 * it.getNb();
-                // }
-            }
-        } else {
-            if (b.getItems() == null) {
-                return 0;
-            }
-
+        if (isDiscountPeriod()) {
             for (int i = 0; i < b.getItems().length; i++) {
                 Item it = b.getItems()[i];
 
@@ -77,30 +41,69 @@ public class ShoppingService {
                 // price += 80 * it.getNb();
                 // }
             }
-        }
 
-        try {
-            if (b.getType().equals("STANDARD_CUSTOMER")) {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
+        } else {
+            for (int i = 0; i < b.getItems().length; i++) {
+                Item it = b.getItems()[i];
+
+                if (it.getType().equals("TSHIRT")) {
+                    p += 30 * it.getNb() * d;
+                } else if (it.getType().equals("DRESS")) {
+                    p += 50 * it.getNb() * d;
+                } else if (it.getType().equals("JACKET")) {
+                    p += 100 * it.getNb() * d;
                 }
-            } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-                if (p > 800) {
-                    throw new Exception("Price (" + p + ") is too high for premium customer");
-                }
-            } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-                if (p > 2000) {
-                    throw new Exception("Price (" + p + ") is too high for platinum customer");
-                }
-            } else {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
+                // else if (it.getType().equals("SWEATSHIRT")) {
+                // price += 80 * it.getNb();
+                // }
             }
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
 
+        checkCustomerMaxPriceCart(b.getType(), p);
         return p;
     }
+
+    // Compute discount for customer
+    private double getCustomerDisount(String customerType) throws Exception {
+        if (customerType.equals("STANDARD_CUSTOMER")) {
+            return 1;
+        } else if (customerType.equals("PREMIUM_CUSTOMER")) {
+            return 0.9;
+        } else if (customerType.equals("PLATINUM_CUSTOMER")) {
+            return 0.5;
+        } else {
+            throw new Exception("Customer type unknown");
+        }
+    }
+
+    private void checkCustomerMaxPriceCart(String customerType, double cartPrice) throws Exception {
+        if (customerType.equals("STANDARD_CUSTOMER")) {
+            if (cartPrice > 200) {
+                throw new Exception("Price (" + cartPrice + ") is too high for standard customer");
+            }
+        } else if (customerType.equals("PREMIUM_CUSTOMER")) {
+            if (cartPrice > 800) {
+                throw new Exception("Price (" + cartPrice + ") is too high for premium customer");
+            }
+        } else if (customerType.equals("PLATINUM_CUSTOMER")) {
+            if (cartPrice > 2000) {
+                throw new Exception("Price (" + cartPrice + ") is too high for platinum customer");
+            }
+        } else {
+            if (cartPrice > 200) {
+                throw new Exception("Price (" + cartPrice + ") is too high for standard customer");
+            }
+        }
+    }
+
+    private boolean isDiscountPeriod() {
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+        return (cal.get(Calendar.DAY_OF_MONTH) < 15 && cal.get(Calendar.DAY_OF_MONTH) > 5
+                && cal.get(Calendar.MONTH) == 5)
+                || (cal.get(Calendar.DAY_OF_MONTH) < 15 && cal.get(Calendar.DAY_OF_MONTH) > 5
+                        && cal.get(Calendar.MONTH) == 0);
+    }
+
 }
